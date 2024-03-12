8e9e85148152887a63db1d163e0716cf4f4abbf2
karan salunkhe <karan.salunkhe@bajajfinserv.in>
2024-03-12 10:06:41 +0530

    prod added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -13,10 +14,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      enabled: !environment.production,
+      registrationStrategy: 'registerWhenStable:30000' // Register when stable or after 30 seconds
     })
   ],
   providers: [],
